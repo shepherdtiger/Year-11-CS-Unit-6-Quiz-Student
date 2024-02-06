444c266efdf0fd8f0193bf7eb649418673fb7ae9
ltst <ltst7232410@gmail.com>
2024-02-06 11:39:15 +0800

    tiger ide

--- src/Address.java
+++ src/Address.java
@@ -1,5 +1,40 @@
 public class Address {
-    public static main{
+        private String district;
+        private String street;
+        private int streetNumber;
 
+        public Address(String district, String street, int streetNumber) {
+            this.district = district;
+            this.street = street;
+            this.streetNumber = streetNumber;
+        }
+
+        public String getDistrict() {
+            return district;
+        }
+
+        public void setDistrict(String district) {
+            this.district = district;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public int getStreetNumber() {
+            return streetNumber;
+        }
+
+        public void setStreetNumber(int streetNumber) {
+            this.streetNumber = streetNumber;
+        }
+
+        @Override
+        public String toString() {
+            return streetNumber + " " + street + ", " + district;
+        }
     }
-}
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,69 @@
 public class Student {
-}
+        private int age;
+        private String firstName;
+        private String lastName;
+        private int year;
+        private Address address;
+
+        public Student(int age, String firstName, String lastName, int year, Address address) {
+            this.age = age;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.year = year;
+            this.address = address;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public Address getAddress() {
+            return address;
+        }
+
+        public void setAddress(Address address) {
+            this.address = address;
+        }
+
+        private void addOneAge() {
+            age++;
+        }
+
+        public int birthday() {
+            addOneAge();
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return firstName + " " + lastName + ", age " + age + ", year " + year + ", " + address.toString();
+        }
+    }
